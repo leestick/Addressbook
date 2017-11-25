9eecc12a481a9300dd383f1c92705fdbc3e4e4cd
leestick <leestick@mail.ru>
2017-11-25 23:04:02 +0300

    Фикстура и переход на pytest

---
+++ test_add_group.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import pytest
+#from group import Group
+from application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_group_add_unittest(app):
+    app.user_login(username="admin", password="secret")
+    app.create_group(name="Group1", header="Header1", footer="Footer1")
+    app.logout()
+
+
+def test_empty_group_add_unittest(app):
+    app.user_login(username="admin", password="secret")
+    app.create_group(name="", header="", footer="")
+    app.logout()
+
+
+
+
