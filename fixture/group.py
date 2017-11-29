0cbd381259917c1395603e0ba4a940f27202fcd3
dmitryh <dmitryh@comita.ru>
2017-11-29 14:31:21 +0300

    выполнен урок 3.5

--- fixture/group.py
+++ fixture/group.py
@@ -8,7 +8,9 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        # check what group page not already loaded (page only with button NEW)
+        if not (wd.currint_url.endswith("/group.php") and len(wd.find_elements_by_name("new"))>0):
+            wd.find_element_by_link_text("groups").click()
 
     def change_field(self, field_name, text):
         wd = self.app.wd
@@ -56,4 +58,8 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.open_groups_page()
 
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
 
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,4 +1,8 @@
 __author__ = 'ioK'
+from model.group import Group
 
 def test_delete_first_group(app):
+    # проверка есть ли хотя бы одна группа
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.delete_first_group()
