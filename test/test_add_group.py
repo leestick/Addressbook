3c0a556cdb099f5f01e87cdfed306fb0629a3a1f
leestick <leestick@mail.ru>
2017-12-27 22:53:28 +0300

    lesson 4.4

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,25 +2,24 @@
 from model.group import Group
 
 
-
 def test_group_add_unittest(app):
     old_groups = app.group.get_group_list()
     group = Group(name="Group1", header="Header1", footer="Footer1")
     app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_empty_group_add_unittest(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+# def test_empty_group_add_unittest(app):
+#     old_groups = app.group.get_group_list()
+#     group = Group(name="", header="", footer="")
+#     app.group.create(group)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) + 1 == len(new_groups)
+#     old_groups.append(group)
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
